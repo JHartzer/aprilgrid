4b6bd0513514276b1705821ff9802e4f69e4817d
Jacob <JacobHartzer@gmail.com>
2025-08-01 13:52:48 -0500

    Add basic test

---
+++ src/test/aprilgrid_test.cpp
@@ -0,0 +1,34 @@
+
+
+#include "aprilgrid.hpp"
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <cmath>
+#include <filesystem>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+
+namespace fs = std::filesystem;
+
+TEST(test_detector, aprilgrid_b2) {
+  auto detector = AprilGrid(cv::aruco::DICT_APRILTAG_36h11, 2, 3, 6, 6, 0.088);
+  auto img = cv::imread(fs::current_path() / "../src/test/aprilgrid_6x6.png", cv::IMREAD_GRAYSCALE);
+
+  cv::Vec3d r_vec;
+  cv::Vec3d t_vec;
+  cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64F);
+  camera_matrix.at<double>(0, 0) = 458.9432286546919;
+  camera_matrix.at<double>(1, 1) = 457.5637533402653;
+  camera_matrix.at<double>(0, 2) = 367.0272509347057;
+  camera_matrix.at<double>(1, 2) = 249.3128033381081;
+  cv::Mat dist_coeffs{0, 0, 0, 0};
+
+  detector.estimatePoseAprilGrid(img, camera_matrix, dist_coeffs, r_vec, t_vec);
+
+  std::cout << r_vec << std::endl;
+  std::cout << t_vec << std::endl;
+}
