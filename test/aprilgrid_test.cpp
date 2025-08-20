3078c784180a8315f31b3a0070c286d687d7d418
Jacob Hartzer <Jacob.Hartzer@shield.ai>
2025-08-20 02:48:21 +0000

    Test all four available dictionaries

--- test/aprilgrid_test.cpp
+++ test/aprilgrid_test.cpp
@@ -33,31 +33,43 @@ class AprilgridDetectorTest : public testing::Test {
   cv::Mat dist_coeffs_ = (cv::Mat_<float>(4, 1) << 0, 0, 0, 0);
 };
 
-TEST_F(AprilgridDetectorTest, draw_aprilgrid_4x6) {
-  auto april_grid = AprilGrid(cv::Size(4, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
+TEST_F(AprilgridDetectorTest, draw_aprilgrid_16h5_4x4) {
+  auto april_grid = AprilGrid(cv::Size(4, 4), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_16h5, 0);
   cv::Mat image_out;
   unsigned int width{400};
   april_grid.draw(width, image_out);
-  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_4x6.png";
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_16h5_4x4.png";
   fs::remove(out_path);
   ASSERT_FALSE(fs::exists(out_path));
   cv::imwrite(out_path, image_out);
   ASSERT_TRUE(fs::exists(out_path));
 }
 
-TEST_F(AprilgridDetectorTest, draw_aprilgrid_6x6) {
-  auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
+TEST_F(AprilgridDetectorTest, draw_aprilgrid_25h9_4x6) {
+  auto april_grid = AprilGrid(cv::Size(4, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_25h9, 0);
   cv::Mat image_out;
   unsigned int width{400};
   april_grid.draw(width, image_out);
-  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_6x6.png";
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_25h9_4x6.png";
   fs::remove(out_path);
   ASSERT_FALSE(fs::exists(out_path));
   cv::imwrite(out_path, image_out);
   ASSERT_TRUE(fs::exists(out_path));
 }
 
-TEST_F(AprilgridDetectorTest, draw_aprilgrid_6x6_occluded) {
+TEST_F(AprilgridDetectorTest, draw_aprilgrid_36h10_6x6) {
+  auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h10, 0);
+  cv::Mat image_out;
+  unsigned int width{400};
+  april_grid.draw(width, image_out);
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_36h10_6x6.png";
+  fs::remove(out_path);
+  ASSERT_FALSE(fs::exists(out_path));
+  cv::imwrite(out_path, image_out);
+  ASSERT_TRUE(fs::exists(out_path));
+}
+
+TEST_F(AprilgridDetectorTest, draw_aprilgrid_36h11_6x6_occluded) {
   auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
   cv::Mat image_out;
   unsigned int width{400};
@@ -77,16 +89,16 @@ TEST_F(AprilgridDetectorTest, draw_aprilgrid_6x6_occluded) {
   // Fill polygon with white color (BGR: 255,255,255)
   cv::fillPoly(image_out, fillContAll, cv::Scalar(255, 255, 255));
 
-  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_6x6_occluded.png";
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_36h11_6x6_occluded.png";
   fs::remove(out_path);
   ASSERT_FALSE(fs::exists(out_path));
   cv::imwrite(out_path, image_out);
   ASSERT_TRUE(fs::exists(out_path));
 }
 
-TEST_F(AprilgridDetectorTest, detect_aprilgrid_4x6) {
-  auto april_grid = AprilGrid(cv::Size(4, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
-  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_4x6.png";
+TEST_F(AprilgridDetectorTest, detect_aprilgrid_16h5_4x4) {
+  auto april_grid = AprilGrid(cv::Size(4, 4), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_16h5, 0);
+  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_16h5_4x4.png";
   auto image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
 
   std::vector<std::vector<cv::Point2f>> corners;
@@ -107,13 +119,13 @@ TEST_F(AprilgridDetectorTest, detect_aprilgrid_4x6) {
       image_out, ids, obj_points, img_points, r_vec, t_vec, camera_matrix_, dist_coeffs_);
   cv::drawFrameAxes(image_out, camera_matrix_, dist_coeffs_, r_vec, t_vec, 0.3);
 
-  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_4x6_out.png";
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_16h5_4x4_out.png";
   cv::imwrite(out_path, image_out);
 }
 
-TEST_F(AprilgridDetectorTest, detect_aprilgrid_6x6) {
-  auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
-  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_6x6.png";
+TEST_F(AprilgridDetectorTest, detect_aprilgrid_25h9_4x6) {
+  auto april_grid = AprilGrid(cv::Size(4, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_25h9, 0);
+  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_25h9_4x6.png";
   auto image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
 
   std::vector<std::vector<cv::Point2f>> corners;
@@ -134,13 +146,13 @@ TEST_F(AprilgridDetectorTest, detect_aprilgrid_6x6) {
       image_out, ids, obj_points, img_points, r_vec, t_vec, camera_matrix_, dist_coeffs_);
   cv::drawFrameAxes(image_out, camera_matrix_, dist_coeffs_, r_vec, t_vec, 0.3);
 
-  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_6x6_out.png";
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_25h9_4x6_out.png";
   cv::imwrite(out_path, image_out);
 }
 
-TEST_F(AprilgridDetectorTest, detect_aprilgrid_6x6_occluded) {
-  auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
-  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_6x6_occluded.png";
+TEST_F(AprilgridDetectorTest, detect_aprilgrid_36h10_6x6) {
+  auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h10, 0);
+  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_36h10_6x6.png";
   auto image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
 
   std::vector<std::vector<cv::Point2f>> corners;
@@ -161,6 +173,33 @@ TEST_F(AprilgridDetectorTest, detect_aprilgrid_6x6_occluded) {
       image_out, ids, obj_points, img_points, r_vec, t_vec, camera_matrix_, dist_coeffs_);
   cv::drawFrameAxes(image_out, camera_matrix_, dist_coeffs_, r_vec, t_vec, 0.3);
 
-  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_6x6_occluded_out.png";
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_36h10_6x6_out.png";
+  cv::imwrite(out_path, image_out);
+}
+
+TEST_F(AprilgridDetectorTest, detect_aprilgrid_36h11_6x6_occluded) {
+  auto april_grid = AprilGrid(cv::Size(6, 6), 0.1, 2, 3, cv::aruco::DICT_APRILTAG_36h11, 0);
+  auto image_path = fs::current_path() / "../../test/assets/aprilgrid_36h11_6x6_occluded.png";
+  auto image = cv::imread(image_path, cv::IMREAD_GRAYSCALE);
+
+  std::vector<std::vector<cv::Point2f>> corners;
+  std::vector<int> ids;
+  april_grid.detectAprilTags(image, corners, ids);
+
+  std::vector<cv::Point3f> obj_points;
+  std::vector<cv::Point2f> img_points;
+  april_grid.matchImagePoints(corners, ids, obj_points, img_points);
+
+  cv::Vec3d r_vec, t_vec;
+  cv::solvePnP(obj_points, img_points, camera_matrix_, dist_coeffs_, r_vec, t_vec);
+
+  cv::Mat image_out;
+  cv::cvtColor(image, image_out, cv::COLOR_GRAY2BGR);
+  april_grid.drawDetectedTags(image_out, ids, img_points);
+  april_grid.drawReprojectionErrors(
+      image_out, ids, obj_points, img_points, r_vec, t_vec, camera_matrix_, dist_coeffs_);
+  cv::drawFrameAxes(image_out, camera_matrix_, dist_coeffs_, r_vec, t_vec, 0.3);
+
+  auto out_path = fs::current_path() / "../../test/assets/aprilgrid_36h11_6x6_occluded_out.png";
   cv::imwrite(out_path, image_out);
 }
